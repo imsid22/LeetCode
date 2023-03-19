86bc2e8e6e1e46fc5cdc242941ad7af74ceed6d5
imsid22 <siddhantd94@gmail.com>
2023-03-19 16:30:29 -0700

    Max subarray - med

--- Medium/Maximum Subarray - 53.py
+++ Medium/Maximum Subarray - 53.py
@@ -1,20 +1,21 @@
 from typing import List
 from dataclasses import dataclass
+import math
 
 @dataclass
 class Solution:
     nums: List[int]
 
     def maxSubArray(self) -> int:
-        maxsum = 0
+        maxsum = -1*math.inf
         for i in range(len(self.nums)):
-            for j in range(len(self.nums)):
+            for j in range(i, len(self.nums)):
                 if sum(self.nums[i:j+1]) > maxsum:
                     maxsum = sum(self.nums[i:j+1])
         return maxsum
 
 
 if __name__ == "__main__":
-    arr = [5,4,-1,7,8]
+    arr = [-2, -1]
     solution = Solution(arr)
     print(solution.maxSubArray())
